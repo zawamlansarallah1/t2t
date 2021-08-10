83dbd1b49f67018de3aa04bc0ecca1336d887a93
zawamlansarallah1 <66381319+zawamlansarallah1@users.noreply.github.com>
2021-08-10 12:24:08 +0200

    Update settings.py

--- settings.py
+++ settings.py
@@ -2,20 +2,20 @@
 
 # https://my.telegram.org/auth and @BotFather
 
-telegram = {'channel': '',  # channel name
-        'token': '',  # your bot token
-        'api_id': 0,
-        'api_hash': '',
-        'phone': ''}
+telegram = {'channel': 'ksjjsjs3',  # channel name
+        'token': '1838315900:AAHi_UdbbdoC14e4UNNFHABB-q-uaE-yIDc',  # your bot token
+        'api_id': 3527330,
+        'api_hash': '062f09a0705e3504511f0c85778aae58',
+        'phone': '+249996025612'}
 
 #get this from https://apps.twitter.com/
 
-twitter = {'consumer_key': '',
-            'consumer_secret': '',
-            'access_token': '',
-            'access_token_secret': ''}
+twitter = {'consumer_key': 'aSKkoyU4ZNcS3PET9OLV2ERzN',
+            'consumer_secret': 'MwoPsGtwvH93gQ9Eu0IcFVLkUyKZxDFq4dOmJjzSUTYH6wFwd1',
+            'access_token': '772065071774179328-svEa24u0sbFByyv2ivoW0RLBa2fGQSx',
+            'access_token_secret': 'oxF9NOx2xkyChoaxN7KQhCZS5Kn9eZ7ViPPaFAc5sLaOe'}
 
-util = {'start': "2018-01-01 00:00:00",     #date from old posts will be tweeted
-        'end': '2018-01-20 00:00:00',       #date until old posts will be tweeted
+util = {'start': "2020-01-01 00:00:00",     #date from old posts will be tweeted
+        'end': '2022-01-20 00:00:00',       #date until old posts will be tweeted
         'format': '%Y-%m-%d %H:%M:%S',      #date format
         'delay': 1}                         #delay time between tweets for client.py
